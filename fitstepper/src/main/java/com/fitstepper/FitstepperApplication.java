5a18fc2400321aca429fd805780c96f3814790fb
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-02-22 09:18:04 +0900

    springのプロジェクトを作成した。

---
+++ fitstepper/src/main/java/com/fitstepper/FitstepperApplication.java
@@ -0,0 +1,13 @@
+package com.fitstepper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitstepperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitstepperApplication.class, args);
+	}
+
+}
