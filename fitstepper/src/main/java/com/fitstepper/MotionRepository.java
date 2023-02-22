fd736e29f630246a3402f74841d1bd239f8f9585
takuma-okamoto-git <s2123080@cco.kanagawa-it.ac.jp>
2023-02-22 17:21:24 +0900

    モーション（えらー）

--- fitstepper/src/main/java/com/fitstepper/MotionRepository.java
+++ fitstepper/src/main/java/com/fitstepper/MotionRepository.java
@@ -1,6 +1,6 @@
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MositonRepository extends JpaRepository<Bui, String>{
+public interface MotionRepository extends JpaRepository<Bui, String>{
 
 }
 
