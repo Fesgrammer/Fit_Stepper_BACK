3022ef967c77917ea5c10fedcb1bdb43158516c7
takuma-okamoto-git <s2123080@cco.kanagawa-it.ac.jp>
2023-02-22 15:48:52 +0900

    部位管理クラスを作成した

---
+++ fitstepper/src/main/java/com/fitstepper/Bui.java
@@ -0,0 +1,20 @@
+package com.fitstepper;
+
+public class Bui{
+
+    private final String bui_name;
+    private final int bui_id;
+
+    public Bui(String text,int ID) {
+        super();
+        this.bui_name = name;
+        this.bui_id=id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getID(){
+        return ID;
+}
