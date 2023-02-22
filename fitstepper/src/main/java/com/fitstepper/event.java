8b0d8d322cf019f6122a7c5860bc4557ab3ec448
takuma-okamoto-git <s2123080@cco.kanagawa-it.ac.jp>
2023-02-22 16:00:23 +0900

    種目管理クラスを作成しました

---
+++ fitstepper/src/main/java/com/fitstepper/event.java
@@ -0,0 +1,20 @@
+package com.fitstepper;
+
+public class Bui{
+
+    private final String event_name;
+    private final int event_id;
+
+    public Bui(String text,int ID) {
+        super();
+        this.event_name = name;
+        this.event_id=id;
+    }
+
+    public String getname() {
+        return event_name;
+    }
+
+    public int getID(){
+        return event_id;
+}
