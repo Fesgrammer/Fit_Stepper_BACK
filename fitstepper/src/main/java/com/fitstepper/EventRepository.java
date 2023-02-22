de93c0e726fcb10ded176e10f00b570061341200
takuma-okamoto-git <s2123080@cco.kanagawa-it.ac.jp>
2023-02-22 17:49:15 +0900

    イベントリポジトリを修正

--- fitstepper/src/main/java/com/fitstepper/EventRepository.java
+++ fitstepper/src/main/java/com/fitstepper/EventRepository.java
@@ -3,6 +3,6 @@ package com.fitstepper;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Bui, String> {
+public interface EventRepository extends JpaRepository<Event, String> {
 
 }
