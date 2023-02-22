a8aedc0391ae374454db20f0bd0fcb2eb415c199
takuma-okamoto-git <s2123080@cco.kanagawa-it.ac.jp>
2023-02-22 16:46:12 +0900

    運動管理コントローラーを作成

---
+++ fitstepper/src/main/java/com/fitstepper/Motuioncontlloer.java
@@ -0,0 +1,18 @@
+
+
+public class Motioncontlloer{
+ 
+    @Autowired
+    private MotionRepository repository;
+
+    @GetMapping(path = "/api/motion/getBuiList")
+    public List<Bui> getBuiList(){
+        return repository.findAll();
+    }
+
+    @GetMapping(path = "/api/motion/getEventList")
+    public List<Event>getEventList(){
+        return repository.findAll();
+    }
+    
+}
