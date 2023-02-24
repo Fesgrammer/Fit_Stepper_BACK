577e20de4da851dc62567073850a78e2b809173a
KHashimoto3 <85172807+KHashimoto3@users.noreply.github.com>
2023-02-24 09:44:32 +0900

    ユーザデータを更新するAPIを作成した。

--- fitstepper/src/main/java/com/fitstepper/User.java
+++ fitstepper/src/main/java/com/fitstepper/User.java
@@ -31,175 +31,172 @@ public class User {
     private int thigh_amt;
     private int calf_level;
     private int calf_amt;
-    
-    public int getuserid() {//userID
+
+    public int getUser_id() {// userID
         return user_id;
     }
 
-    public void setUserId(int user_id) {
+    public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
 
-    public String getname() {//name
+    public String getName() {// name
         return name;
     }
 
-    public void setname(String name) {
-        this.name =name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getpass() {//pass
+    public int getPass() {// pass
         return pass;
     }
 
-    public void setpass(int pass) {
+    public void setPass(int pass) {
         this.pass = pass;
     }
 
-    public int getjouwan2_level() {//jouwan2
+    public int getJouwan2_level() {// jouwan2
         return jouwan2_level;
     }
 
-    public void setjouwan2_level(int jouwan2_level) {
+    public void setJouwan2_level(int jouwan2_level) {
         this.jouwan2_level = jouwan2_level;
     }
-    
-    public int getjouwan2_amt() {
+
+    public int getJouwan2_amt() {
         return jouwan2_amt;
     }
 
-    public void setjouwan2_amt(int jouwan2_amt) {
+    public void setJouwan2_amt(int jouwan2_amt) {
         this.jouwan2_amt = jouwan2_amt;
     }
-    
-    public int getjouwan3_level() {//jouwan3
+
+    public int getJouwan3_level() {// jouwan3
         return jouwan3_level;
     }
 
-    public void setjouwan3_level(int jouwan3_level) {
+    public void setJouwan3_level(int jouwan3_level) {
         this.jouwan3_level = jouwan3_level;
     }
 
-
-    public int getjouwan3_amt() {
+    public int getJouwan3_amt() {
         return jouwan3_amt;
     }
 
-    public void setjouwan3_amt(int jouwan3_amt) {
+    public void setJouwan3_amt(int jouwan3_amt) {
         this.jouwan3_amt = jouwan3_amt;
     }
 
-    public int getchestlevel() {//chest
+    public int getChest_level() {// chest
         return chest_level;
     }
 
-    public void setchestlevel(int chest_level) {
+    public void setChest_level(int chest_level) {
         this.chest_level = chest_level;
     }
 
-    public int getchestamt() {
+    public int getChest_amt() {
         return chest_amt;
     }
 
-    public void setchestamt(int chest_amt) {
+    public void setChest_amt(int chest_amt) {
         this.chest_amt = chest_amt;
     }
 
-    public int getadslevel() {//chest
+    public int getAds_level() {// chest
         return ads_level;
     }
 
-    public void setadslevel(int ads_level) {
+    public void setAds_level(int ads_level) {
         this.ads_level = ads_level;
     }
 
-    public int getadsamt() {
+    public int getAds_amt() {
         return ads_amt;
     }
 
-    public void setadsamt(int ads_amt) {
+    public void setAds_amt(int ads_amt) {
         this.ads_amt = ads_amt;
     }
 
-    public int getshoulderlevel() {//shouder
+    public int getShoulder_level() {// shouder
         return shoulder_level;
     }
 
-    public void setshoulderlevel(int shoulder_level) {
+    public void setShoulder_level(int shoulder_level) {
         this.shoulder_level = shoulder_level;
     }
 
-    public int getshoulderamt() {
+    public int getShoulder_amt() {
         return shoulder_amt;
     }
 
-    public void setshoulderamt(int shoulder_amt) {
+    public void setShoulder_amt(int shoulder_amt) {
         this.shoulder_amt = shoulder_amt;
     }
 
-    public int getbacklevel() {//back
+    public int getBack_level() {// back
         return back_level;
     }
 
-    public void setbacklevel(int back_level) {
+    public void setBack_level(int back_level) {
         this.back_level = back_level;
     }
 
-    public int getbackamt() {
+    public int getBack_amt() {
         return back_amt;
     }
 
-    public void setbackamt(int back_amt) {
-        this.back_amt= back_amt;
+    public void setBack_amt(int back_amt) {
+        this.back_amt = back_amt;
     }
 
-    
-    public int gethiplevel() {//hip
+    public int getHip_level() {// hip
         return hip_level;
     }
 
-    public void sethiplevel(int hip_level) {
+    public void setHip_level(int hip_level) {
         this.hip_level = hip_level;
     }
 
-    public int gethipamt() {
+    public int getHip_amt() {
         return hip_amt;
     }
 
-    public void sethipamt(int hip_amt) {
+    public void setHip_amt(int hip_amt) {
         this.hip_amt = hip_amt;
     }
 
-    public int getthighlevel() {//thigh
+    public int getThigh_level() {// thigh
         return thigh_level;
     }
 
-    public void setthighlevel(int thigh_level) {
+    public void setThigh_level(int thigh_level) {
         this.thigh_level = thigh_level;
     }
 
-    
-    public int getthighamt() {
+    public int getThigh_amt() {
         return thigh_amt;
     }
 
-    public void setthighamt(int thigh_amt) {
+    public void setThigh_amt(int thigh_amt) {
         this.thigh_amt = thigh_amt;
     }
 
-   public int getcalflevel() {//calf
+    public int getCalf_level() {// calf
         return calf_level;
     }
 
-    public void setcalflevel(int calf_level) {
+    public void setCalf_level(int calf_level) {
         this.calf_level = calf_level;
     }
 
-    public int getcalfamt() {
+    public int getCalf_amt() {
         return calf_amt;
     }
 
-    public void setcalfatm(int calf_amt) {
+    public void setCalf_amt(int calf_amt) {
         this.calf_amt = calf_amt;
     }
 
--- fitstepper/src/main/java/com/fitstepper/UserController.java
+++ fitstepper/src/main/java/com/fitstepper/UserController.java
@@ -11,24 +11,111 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class UserController {
-    @Autowired
-    private UserRepository repository;
+        @Autowired
+        private UserRepository repository;
 
-    @RequestMapping("api/user/login")
-    public String login(@RequestParam("user_id") int userId){
-      
-            return "YES";
-    }
+        @RequestMapping("api/user/login")
+        public String login(@RequestParam("user_id") int userId) {
 
-    @RequestMapping("api/user/add")
-    public String add(@RequestParam("user_id") int userId){
-      
-            return "YES";
-    }
+                return "YES";
+        }
 
-    @RequestMapping("api/user/getLevel")//実装できるかわからなそう？
-    public int getuserlevel(@RequestParam("user_id") int userId){
-      
-            return 1;
-    }
+        @RequestMapping("api/user/add")
+        public String add(@RequestParam("user_id") int userId) {
+
+                return "YES";
+        }
+
+        @RequestMapping("api/user/getLevel") // 実装できるかわからなそう？
+        public int getuserlevel(@RequestParam("user_id") int userId) {
+
+                return 1;
+        }
+
+        @RequestMapping(value = "api/user/updateData", method = { RequestMethod.POST })
+        public String updateData(@RequestBody UpdateUsrDataIn uddi) {
+                User usrBefore = repository.findById(uddi.getUser_id());
+                User usr = new User();
+
+                usr = usrBefore;
+                Boolean updateFlag = false;
+
+                switch (uddi.getBui_id()) {
+                        case 1:
+                                if (usrBefore.getJouwan2_level() < uddi.getNew_level()) {
+                                        usr.setJouwan2_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setJouwan2_amt(uddi.getTotal_amount());
+                                break;
+                        case 2:
+                                if (usrBefore.getJouwan3_level() < uddi.getNew_level()) {
+                                        usr.setJouwan3_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setJouwan3_amt(uddi.getTotal_amount());
+                                break;
+                        case 3:
+                                if (usrBefore.getChest_level() < uddi.getNew_level()) {
+                                        usr.setChest_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setChest_amt(uddi.getTotal_amount());
+                                break;
+                        case 4:
+                                if (usrBefore.getAds_level() < uddi.getNew_level()) {
+                                        usr.setAds_level(uddi.getNew_level());
+                                        ;
+                                        updateFlag = true;
+                                }
+                                usr.setAds_amt(uddi.getTotal_amount());
+                                break;
+                        case 5:
+                                if (usrBefore.getShoulder_level() < uddi.getNew_level()) {
+                                        usr.setShoulder_level(uddi.getNew_level());
+                                        ;
+                                        updateFlag = true;
+                                }
+                                usr.setShoulder_amt(uddi.getTotal_amount());
+                                break;
+                        case 6:
+                                if (usrBefore.getBack_level() < uddi.getNew_level()) {
+                                        usr.setBack_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setBack_amt(uddi.getTotal_amount());
+                                break;
+                        case 7:
+                                if (usrBefore.getHip_level() < uddi.getNew_level()) {
+                                        usr.setHip_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setHip_amt(uddi.getTotal_amount());
+                                break;
+                        case 8:
+                                if (usrBefore.getThigh_level() < uddi.getNew_level()) {
+                                        usr.setThigh_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setThigh_amt(uddi.getTotal_amount());
+                                break;
+                        case 9:
+                                if (usrBefore.getCalf_level() < uddi.getNew_level()) {
+                                        usr.setCalf_level(uddi.getNew_level());
+                                        updateFlag = true;
+                                }
+                                usr.setCalf_amt(uddi.getTotal_amount());
+                                break;
+
+                }
+
+                repository.save(usr);
+
+                if (updateFlag) {
+                        return "UP";
+                } else {
+                        return "KEEP";
+                }
+
+        }
 }
